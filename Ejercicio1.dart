2cd2a52a4afa851047cf469a73406917ec8337ba
SerTroyan111 <sergio.inestroza@unah.hn>
2025-10-05 17:27:39 -0600

    Arreglos hechos

--- Ejercicio1.dart
+++ Ejercicio1.dart
@@ -1,49 +1,44 @@
 //Sergio Rolando Inestroza Amaya 20182002621
 
-// void main()
-// {
-//   List <Object> empleados = [Empleado('Sergio', 10, 15000), Empleado('German', 20, 16000), Empleado('Rocio', 30, 12000), Empleado('Aaron', 40, 14000)];
-//   // final E1 = Empleado('Sergio', 10, 15000);
-//   // final E2 = Empleado('German', 20, 16000);
-//   // final E3 = Empleado('Rocio', 30, 12000);
-//   // final E4 = Empleado('Aaron', 40, 14000);
+void main()
+{
+  List <Object> empleados = [Empleado('Sergio', 10, 15000), Empleado('German', 20, 16000), Empleado('Rocio', 30, 12000), Empleado('Aaron', 40, 14000)];
+  // final E1 = Empleado('Sergio', 10, 15000);
+  // final E2 = Empleado('German', 20, 16000);
+  // final E3 = Empleado('Rocio', 30, 12000);
+  // final E4 = Empleado('Aaron', 40, 14000);
 
-//   print(empleados);
-// }
-
-// class Empleado
-// {
-//   final String nombre;
-//   final int id;
-//   final double salario;
-
-//   Empleado(this.nombre, this.id, this.salario)
-//   {}
-// }
-
-void main() {
-  List<Empleado> empleados = [
-    Empleado('Sergio', 10, 15000),
-    Empleado('German', 20, 16000),
-    Empleado('Rocio', 30, 12000),
-    Empleado('Aaron', 40, 14000),
-  ];
-
-  print("Lista de empleados:");
-  for (var empleado in empleados) {
+  print("Salarios antes del aumento");
+  for (var empleado in empleados)
+  {
     print(empleado);
   }
 }
 
-class Empleado {
+class Empleado
+{
   final String nombre;
   final int id;
   double salario;
 
-  Empleado(this.nombre, this.id, this.salario);
+  Empleado(this.nombre, this.id, this.salario)
+  {}
 
   @override
-  String toString() {
+  String toString() 
+  {
     return "Empleado: $nombre, ID: $id, Salario: $salario";
+  }
+}
+
+void aumentarSalario(Empleado empleados, {required double porcentaje})
+{
+  if(porcentaje <= 0)
+  {
+    print("El aumento de salario debe ser un porcentaje mayor que cero");
+  }
+  else
+  {
+
   }
 }
