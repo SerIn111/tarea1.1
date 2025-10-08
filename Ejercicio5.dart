f2fb68efe6deac13f7d56b8fbc2ab46c947994d6
SerTroyan111 <sergio.inestroza@unah.hn>
2025-10-08 12:36:57 -0600

    Ejercicio 5 terminado

---
+++ Ejercicio5.dart
@@ -0,0 +1,60 @@
+//Sergio Rolando Inestroza Amaya 20182002621
+
+void main()
+{
+  Map<String, List<double>> notasEstudiantes = 
+  {
+    'Sergio': [70, 80, 90, 100],
+    'Jeffry' : [90, 80, 95, 100],
+    'Nelson' : [65, 65, 65, 65]
+  };
+
+  print('Promedio de Sergio: ${calcularPromedio(notasEstudiantes['Sergio']!)}');
+  print('Promedio de Jeffry: ${calcularPromedio(notasEstudiantes['Jeffry']!)}');
+  print('Promedio de Nelson: ${calcularPromedio(notasEstudiantes['Nelson']!)}');
+  print('');
+  print('El mejor estudiante es ${obtenerMejorEstudiante(notasEstudiantes)} con un promedio de ${calcularPromedio(notasEstudiantes[obtenerMejorEstudiante(notasEstudiantes)]!)}');
+}
+
+double calcularPromedio(List<double> notas)
+{
+  double prom = 0;
+  for(int i = 0; i <= notas.length - 1; i++)
+  {
+    prom += notas[i];
+  }
+  prom = prom / (notas.length);
+  return prom;
+}
+
+
+String obtenerMejorEstudiante(Map<String, List<double>> notasEstudiantes)
+{
+  List<String> estudiantes = [];
+  List<double> notas = [];
+  String mejorestudiante = '';
+  double promedioactual, promediomasalto = 0;
+  for(int i = 0; i < notasEstudiantes.length; i++) //Para extraer los estudiantes del mapa
+  {
+    estudiantes.add(notasEstudiantes.keys.toList().elementAt(i));
+  }
+
+  for(int j = 0; j < estudiantes.length; j++)
+  {
+    notas.clear();
+    promedioactual = 0;
+    for(int k = 0; k < notasEstudiantes[estudiantes[j]]!.toList().length; k++) //Recorrer los valores de
+    {
+      // notas.add(notasEstudiantes[estudiantes[j]]!.toList().elementAt(k));
+      promedioactual += notasEstudiantes[estudiantes[j]]!.toList().elementAt(k);
+    }
+    promedioactual = promedioactual / notasEstudiantes[estudiantes[j]]!.toList().length;
+    if(promedioactual > promediomasalto)
+    {
+      promediomasalto = promedioactual;
+      mejorestudiante = estudiantes.elementAt(j);
+    }
+    
+  }
+  return mejorestudiante;
+}
